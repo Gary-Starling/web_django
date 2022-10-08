1972e01b937f683b8c31111d2fcf7133ef01a74c
Gary-Starling <skvorec92@mail.ru>
2022-10-08 12:29:50 +0300

    theme css page

--- my_web_proj/my_web_proj/settings.py
+++ my_web_proj/my_web_proj/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', #js,img,cs look STATIC_URL 
     'main', #reg my app 'main'
 ]
 
@@ -118,7 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' #Path
+
+''' dirs static resources '''
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- my_web_proj/my_web_proj/urls.py
+++ my_web_proj/my_web_proj/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include # for add url patterns
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),   # general page
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
