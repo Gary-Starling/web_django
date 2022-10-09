ffb94167c965b62fb762259e73e59d7219ac20bc
Gary-Starling <skvorec92@mail.ru>
2022-10-09 15:15:27 +0300

    database page and users

--- my_web_proj/database/views.py
+++ my_web_proj/database/views.py
@@ -1,9 +1,12 @@
 import re
 from django.shortcuts import render
+from .models import UsersInTeam
+
 
 # Create your views here.
 
 
 '''db start page'''
 def db_start(req):
-    return render(req,'database/db.html')
+    users = UsersInTeam.objects.order_by('age')
+    return render(req,'database/db.html', {'users':users}) #Передавать нужно по ключу
