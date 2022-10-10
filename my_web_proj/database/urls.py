e10eb4ee31c88b8f7d04fb608680d4ef2fd792a2
Gary-Starling <skvorec92@mail.ru>
2022-10-10 16:23:49 +0300

    Dynamic page users. TODO: write Readme how it work

--- my_web_proj/database/urls.py
+++ my_web_proj/database/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.db_start, name='db_start'),
-    path('add', views.add, name='add')
+    path('add', views.add, name='add'),
+    path('<int:pk>', views.NewUserView.as_view(), name='users')
 ]
--- my_web_proj/database/views.py
+++ my_web_proj/database/views.py
@@ -2,14 +2,14 @@ import re
 from django.shortcuts import render,redirect
 from .models import UsersInTeam
 from .forms import UserInTeamForm
-
+from django.views.generic import DetailView
 # Create your views here.
-
 
 '''db start page'''
 def db_start(req):
     users = UsersInTeam.objects.order_by('age')
     return render(req,'database/db.html', {'users':users}) #Передавать нужно по ключу
+
 
 
 '''add new user'''
@@ -35,3 +35,11 @@ def add(req):
     }
 
     return render(req,'database/add.html', data) #Передавать нужно по ключу
+
+
+
+class NewUserView(DetailView):
+    model = UsersInTeam
+    template_name = 'database/user_detail.html'
+    context_object_name = 'user_key'
+
