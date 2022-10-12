5e79035b935cf2ef25f3295a1e54d737797a5c7e
Gary-Starling <skvorec92@mail.ru>
2022-10-12 11:39:48 +0300

    fix

--- my_web_proj/my_web_proj/settings.py
+++ my_web_proj/my_web_proj/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR)
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6+ma-+$w*4h&f(mjh^t-elr(7&a^=eyic+4-_71=+9!vc@84(6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,7 +128,8 @@ STATIC_URL = '/static/' #Path
 
 ''' dirs static resources '''
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'main/static')
+    #BASE_DIR / "static",
 ]
 
 # Default primary key field type
