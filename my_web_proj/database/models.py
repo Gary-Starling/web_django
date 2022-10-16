ecb5baeb4f4213da1d0d7cc4261c38c35eca07f2
Gary-Starling <skvorec92@mail.ru>
2022-10-16 10:42:27 +0300

    new in database

--- my_web_proj/database/models.py
+++ my_web_proj/database/models.py
@@ -1,5 +1,6 @@
 from tabnanny import verbose
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,6 +10,8 @@ class UsersInTeam(models.Model):  # Наследуем от models.Model
     name = models.CharField('Username', max_length=100)
     age = models.IntegerField('Age')
     date_reg = models.DateTimeField('Date')
+    #Наследуемся отсюда from django.contrib.auth.models import User(Все пользователи)/при удалении пользователя, удаляются все его записи
+    who_add = models.ForeignKey(User, verbose_name='user', on_delete=models.CASCADE) #id того кто добавил
 
     def __str__(self):
         return self.name
